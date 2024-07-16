e3d74c82d7fa68c1a7feeed5e7a5b7172fca6296
Grilindor <marcor91@hotmail.fr>
2024-07-16 10:27:08 +0200

    task 1. Block Scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,8 @@
+export default function taskBlock(trueOrFalse) {
+  /* modify the variables so that the variables aren’t
+   overwritten inside the conditional block*/
+  const task = false;
+  const task2 = true;
+
+  return [task, task2];
+}
