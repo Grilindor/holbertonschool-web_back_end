8b19cac5ecf165cb3c00405d360d1ff3272020a5
Grilindor <marcor91@hotmail.fr>
2024-07-16 13:25:22 +0200

    task 4. Rest parameter syntax for functions

---
+++ ES6_basic/4-rest-parameter.js
@@ -0,0 +1,8 @@
+export default function returnHowManyArguments(...args) {
+  return args.length;
+}
+
+/*
+La syntaxe ...args permet de capturer tous les arguments
+passés à la fonction dans un tableau nommé args.
+*/
