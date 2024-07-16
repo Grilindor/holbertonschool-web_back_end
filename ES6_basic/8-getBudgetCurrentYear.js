3d42bdf42cf1946cf34e11c63301e5e533a5ef83
Grilindor <marcor91@hotmail.fr>
2024-07-16 15:44:43 +0200

    corectif

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,15 +4,9 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const currentYear = getCurrentYear();
   return {
-    [`income-${currentYear}`]: income,
-    [`gdp-${currentYear}`]: gdp,
-    [`capita-${currentYear}`]: capita,
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
 }
-
-/*créer une variable const qui reprend les variable*/
-/*En stockant le résultat de getCurrentYear() dans une variable currentYear,
-nous évitons d'appeler la fonction plusieurs fois,
- ce qui rend le code plus efficace.*/
