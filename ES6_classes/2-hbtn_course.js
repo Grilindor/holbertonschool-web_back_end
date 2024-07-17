5a42f29d111fc11117f45c2301795a86e7576506
Grilindor <marcor91@hotmail.fr>
2024-07-17 18:44:52 +0200

    correction

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,12 +1,21 @@
 class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (!Array.isArray(students) || students.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
   }
 
   // Getter
-  get gname() {
+  get name() {
     return this._name;
   }
 
