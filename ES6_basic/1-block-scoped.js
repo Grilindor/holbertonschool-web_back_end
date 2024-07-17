95f8d181a2f66ee0f74e0698d4b91f0550d0f935
Grilindor <marcor91@hotmail.fr>
2024-07-17 10:20:31 +0200

    remmove the comment, it's stupid the cheker

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,4 @@
 export default function taskBlock() {
-  /* modify the variables so that the variables aren’t
-   overwritten inside the conditional block*/
   const task = false;
   const task2 = true;
 
