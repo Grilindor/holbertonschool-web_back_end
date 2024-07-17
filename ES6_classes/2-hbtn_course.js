89c14abfa54367b932fa65a2a5cd1be0e5a4f9ae
Grilindor <marcor91@hotmail.fr>
2024-07-17 18:52:30 +0200

    test

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -18,33 +18,33 @@ class HolbertonCourse {
     return this._name;
   }
 
-    set name(nname) {
-      if (typeof nname !== 'string') {
-          throw new TypeError('Name must be a string');
-      }
-      this._name = nname;
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
   }
 
   get length() {
-      return this._length;
+    return this._length;
   }
 
-  set length(nlength) {
-      if (typeof nlength !== 'number') {
-          throw new TypeError('Length must be a number');
-      }
-      this._length = nlength;
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
   }
 
   get students() {
-      return this._students;
+    return this._students;
   }
 
-  set students(nstudent) {
-      if (!Array.isArray(nstudent) || nstudent.some((student) => typeof student !== 'string')) {
-          throw new TypeError('Students must be an array of strings');
-      }
-      this._students = nstudent;
+  set students(newStudents) {
+    if (!Array.isArray(newStudents) || newStudents.some((student) => typeof student !== 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
   }
 }
 
