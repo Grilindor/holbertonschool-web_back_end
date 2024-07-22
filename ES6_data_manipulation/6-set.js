01d9f4f25e83b807597eb65f94bcace896b88c27
Grilindor <marcor91@hotmail.fr>
2024-07-22 16:04:32 +0200

    task 6. Set data structure

---
+++ ES6_data_manipulation/6-set.js
@@ -0,0 +1,6 @@
+export default function setFromArray(array) {
+  /* Create and return a new Set containing the elements of the given array.
+  Duplicates in the table are automatically removed
+  Because a Set can only contain unique values. */
+  return new Set(array);
+}
