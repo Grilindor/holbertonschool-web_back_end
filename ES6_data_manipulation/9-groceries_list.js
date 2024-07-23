a0edd54b9814e017e5ef2a393bc9a3378f4c5e2a
Grilindor <marcor91@hotmail.fr>
2024-07-23 10:11:17 +0200

    task 9. Map data structure

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+export default function groceriesList() {
+  // Create a new Map using an array of key-value pairs
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+
+  return groceries;
+}
