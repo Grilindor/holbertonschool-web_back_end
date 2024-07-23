99cea63b1bb6d098d694bc24a2a468612a64e5e7
Grilindor <marcor91@hotmail.fr>
2024-07-23 10:53:16 +0200

    correctif of format ES6

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,10 +2,8 @@ export default function cleanSet(set, startString) {
   if (!startString || typeof startString !== 'string') return '';
   // Convert Set to array
   return Array.from(set)
-    // Filter values ​​that start with startString
     .filter((element) => typeof element === 'string' && element.startsWith(startString))
     // Extract the part after startString
     .map((element) => element.slice(startString.length))
-    // Join values ​​with a separator
     .join('-');
 }
