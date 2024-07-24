46720796c9b06672787a1d0cc5eef95ff4e4b895
Grilindor <marcor91@hotmail.fr>
2024-07-24 09:46:42 +0200

    correction, format and wrong word at some place

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,10 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return result.map((result) => ({
     status: result.status,
-    value: result.status === 'rejected' ? result.value : `Error: ${result.reason.message}`,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
   }));
 }
