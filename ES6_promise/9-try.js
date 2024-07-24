e0e08a149279ba16381c787ca4d21f8ba36843df
Grilindor <marcor91@hotmail.fr>
2024-07-24 11:08:41 +0200

    change error for ${error.name}

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(`Error: ${error.message}`);
+    queue.push(`${error.name}: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
