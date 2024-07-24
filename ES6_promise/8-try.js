3121a523289f3ed552309216dca8ebf7f6eb8a4e
Grilindor <marcor91@hotmail.fr>
2024-07-24 10:27:53 +0200

    task 8. Throw an error

---
+++ ES6_promise/8-try.js
@@ -0,0 +1,7 @@
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    // throw a new error with a personal message
+    throw new Error('cannot divide by 0');
+  }
+  return numerator/denominator;
+}
