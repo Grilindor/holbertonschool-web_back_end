d25f42ad63e2ef223ea10cf8af49c1c814362b28
Grilindor <marcor91@hotmail.fr>
2024-07-24 10:28:43 +0200

    correctif for format

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -3,5 +3,5 @@ export default function divideFunction(numerator, denominator) {
     // throw a new error with a personal message
     throw new Error('cannot divide by 0');
   }
-  return numerator/denominator;
+  return numerator / denominator;
 }
