90a24293a8bf62cdb7f0876b71fe7d5cdeae7784
Grilindor <marcor91@hotmail.fr>
2024-07-24 11:17:43 +0200

    add comment

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,11 +1,15 @@
 export default function guardrail(mathFunction) {
+  // create a array queue
   const queue = [];
   try {
+    // execut mathfunction, add result to the end of queue
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
+    // if error add personal error a the end of queue
     queue.push(`${error.name}: ${error.message}`);
   } finally {
+    // add personal message at the end
     queue.push('Guardrail was processed');
   }
 
