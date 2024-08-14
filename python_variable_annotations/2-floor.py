cb0ba869453ed455bd3dcb23ac522b48a27665f4
Grilindor <marcor91@hotmail.fr>
2024-08-14 09:53:33 +0200

    pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python3
+""" 2. Basic annotations - floor """
 import math
+
+
 def floor(n: float) -> int:
-    # returns the rounding of n
+    """ returns the rounding of n """
     return math.floor(n)
