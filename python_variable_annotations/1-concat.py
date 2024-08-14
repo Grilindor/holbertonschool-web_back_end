043a87bbd96f95bd4534412076fdf21828720856
Grilindor <marcor91@hotmail.fr>
2024-08-14 09:40:13 +0200

    add in the good directory

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,3 @@
+def concat(str1: str, str2: str) -> str:
+    # concat str1 and str2 in a new str
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,4 @@
+import math
+def floor(n: float) -> int:
+    # returns the rounding of n
+    return math.floor(n)
