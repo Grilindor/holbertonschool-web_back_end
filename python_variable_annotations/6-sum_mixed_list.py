d9f30556d267bf929e7ebfa94fc89d992529fcb8
Grilindor <marcor91@hotmail.fr>
2024-08-14 12:00:57 +0200

    correctif

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """ return the sum of the miixed list """
     return sum(mxd_lst)
