e19563be6cda755c97ea904f5287303fe14561d7
Grilindor <marcor91@hotmail.fr>
2024-08-14 10:42:00 +0200

    task5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 5. Complex types - list of floats """
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ return the sum of the float list """
+    return sum(input_list)
