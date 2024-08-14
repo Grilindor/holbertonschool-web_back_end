33d3a28ab6cab7759e508abdb180abeafd7accf5
Grilindor <marcor91@hotmail.fr>
2024-08-14 09:51:59 +0200

    pycodestyle

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+""" 1. Basic annotations - concat """
+
+
 def concat(str1: str, str2: str) -> str:
-    # concat str1 and str2 in a new str
+    """ concat str1 and str2 in a new str """
     return str1 + str2
