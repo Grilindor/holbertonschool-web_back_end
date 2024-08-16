84934dda731da17b89efa3c9fe97fbacdc05f662
Grilindor <marcor91@hotmail.fr>
2024-08-16 15:03:57 +0200

    more compact

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -5,6 +5,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """ asyncronique function to creat a list of float
     using task_wait_random for n use """
@@ -12,10 +13,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
 
     delays = await asyncio.gather(*listing)
 
-    """ sort the result without sort() """
-    for i in range(len(delays)):
-        for j in range(i + 1, len(delays)):
-            if delays[i] > delays[j]:
-                delays[i], delays[j] = delays[j], delays[i]
-
-    return delays
+    return sorted(delays)
