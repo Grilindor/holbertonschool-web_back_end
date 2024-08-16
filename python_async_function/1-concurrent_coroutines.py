0a248b704e8da0681df31df73748d65fdc60a70e
Grilindor <marcor91@hotmail.fr>
2024-08-16 14:50:04 +0200

    i dont understand why

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,3 +9,21 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """ return async routine that takes in 2 int arguments """
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(delays)
+
+
+"""async def wait_n(n: int, max_delay: int) -> List[float]:
+     # asyncronique function to creat a list of float
+    using wait_random for n use
+    listing = []
+    for _ in range(n):
+        listing.append(asyncio.create_task(wait_random(max_delay)))
+
+    delays = await asyncio.gather(*listing)
+
+    # sort the result without sort()
+    for i in range(len(delays)):
+        for j in range(i + 1, len(delays)):
+            if delays[i] > delays[j]:
+                delays[i], delays[j] = delays[j], delays[i]
+
+    return delays"""
