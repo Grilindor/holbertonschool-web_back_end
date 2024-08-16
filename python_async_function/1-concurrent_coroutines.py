88404da949d2e4cb5cbaec90df77bc5c1ff5c7d9
Grilindor <marcor91@hotmail.fr>
2024-08-16 14:45:39 +0200

    more compact and efficiant, normaly

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,18 +6,22 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ asyncronique function to creat a list of float
-    using wait_random for n use"""
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
+
+"""async def wait_n(n: int, max_delay: int) -> List[float]:
+     # asyncronique function to creat a list of float
+    using wait_random for n use
     listing = []
     for _ in range(n):
         listing.append(asyncio.create_task(wait_random(max_delay)))
 
     delays = await asyncio.gather(*listing)
 
-    """ sort the result without sort() """
+    # sort the result without sort()
     for i in range(len(delays)):
         for j in range(i + 1, len(delays)):
             if delays[i] > delays[j]:
                 delays[i], delays[j] = delays[j], delays[i]
 
-    return delays
+    return delays"""
