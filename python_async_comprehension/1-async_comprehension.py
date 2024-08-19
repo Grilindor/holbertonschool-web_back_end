aadc13ba2d90c34c175a12044d29a06d4e39dc10
Grilindor <marcor91@hotmail.fr>
2024-08-19 10:56:27 +0200

    task1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 1. Async Comprehensions """
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """
+    Returns a list of values from async_generator using asynchronous
+    comprehension.
+    This function collects and returns all values yielded by the
+    async_generator function in a list, processed asynchronously.
+    """
+    return [i async for i in async_generator()]
