4be85c71f3d7267ed6953e7a1cbc24deaf1aa6c5
Grilindor <marcor91@hotmail.fr>
2024-08-19 12:37:30 +0200

    add import typing and type return

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """ 1. Async Comprehensions """
 import asyncio
+import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> typing.List[float]:
     """
     Returns a list of values from async_generator using asynchronous
     comprehension.
