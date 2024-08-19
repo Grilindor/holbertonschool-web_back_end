fa9d0a3a8c4e5192d5ef97f99e89ecc527ed7416
Grilindor <marcor91@hotmail.fr>
2024-08-19 12:33:58 +0200

    correctif add type of return

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ 0. Async Generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ wait 1sec befor excut random nummber and return it """
     for i in range(10):
         await asyncio.sleep(1)
