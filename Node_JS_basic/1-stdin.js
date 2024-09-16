6d77a17e42e4e0a5f4d7b822738db17f49baab18
Grilindor <marcor91@hotmail.fr>
2024-09-16 14:14:32 +0200

    task1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,17 @@
+
+console.log("Welcome to Holberton School, what is your name?")
+// active stdin
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+
+// lisen enter from user
+process.stdin.on('data', function(data) {
+  const name = data.trim();
+  console.log(`Your name is: ${name}` );
+
+
+  console.log('This important software is now closing');
+  process.exit();
+
+});
