ea11f01636d0ac929f34eb156959bc6e44aafcba
Grilindor <marcor91@hotmail.fr>
2024-09-17 12:07:22 +0200

    task4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http')
+
+const app = http.createServer((req, res) =>{
+
+  res.writeHead(200, { 'content-Type': 'text/plain' });
+
+  res.end('Hello Holberton School!');
+
+});
+
+app.listen(1245);
+
+module.exports = app;
