18675fea8497f1321cd10936d55b65fa9f9b7679
Grilindor <marcor91@hotmail.fr>
2024-09-17 17:12:37 +0200

    task6

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
+
+
+module.exports= app;
