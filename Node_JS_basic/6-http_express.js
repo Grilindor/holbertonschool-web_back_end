88f6a6d2270a1c32b7a71f2c5d3987256364e9f2
Grilindor <marcor91@hotmail.fr>
2024-09-18 11:19:52 +0200

    test

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 const app = express();
-
 const port = 1245;
 
 app.get('/', (req, res) => {
@@ -11,6 +10,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Serveur en écoute sur le port ${port}`);
 });
-
 
 module.exports= app;
