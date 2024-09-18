5423bf9d9cfaaf4f5ce4d756206794576b2ae67d
Grilindor <marcor91@hotmail.fr>
2024-09-18 09:42:13 +0200

    correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,14 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log("Welcome to Holberton School, what is your name?")
 
-// Active stdin
+// active stdin
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
-// Fonction principale
-function askName() {
-  // Écoute l'entrée de l'utilisateur
-  process.stdin.on('data', function(data) {
-    const name = data.trim();
-    console.log(`Your name is: ${name}`);
+// lisen enter from user
+process.stdin.on('data', function(data) {
+  const name = data.trim();
+  console.log(`Your name is: ${name}` );
 
-    console.log('This important software is now closing');
-    process.exit();
-  });
-}
-
-// Exporte la fonction
-module.exports = askName;
+  console.log('This important software is now closing');
+  process.exit();
+});
